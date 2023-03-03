40e79076a5b076c86d8a36cefea544c112319df4
Joshua Keith <joshualeekeith@outlook.com>
2023-03-02 21:34:04 -0600

    Through Button Functionality

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your code here
+let table = document.getElementById("table");
+// // function makeRow() {
+//   let tr = document.createElement("tr");
+//   table.appendChild("tr");
+// }
+//makeRow();
+function makeRow() {
+  let tr = document.createElement("tr");
+  for (let i = 0; i < 20; i++) {
+    let td = document.createElement("td");
+    tr.appendChild(td);
+  }
+  table.appendChild(tr);
+}
+let addRow = document.getElementById("add-row");
+addRow.addEventListener("click", makeRow, false);
