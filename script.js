44633634e38b6fa0c1c231315e8057d893369c2b
Jeremy Stanley <jstanley490@gmail.com>
2023-03-02 22:23:21 -0600

    now we are at choosing a color

--- script.js
+++ script.js
@@ -5,13 +5,29 @@ let table = document.getElementById("table");
 //   table.appendChild("tr");
 // }
 //makeRow();
+
 function makeRow() {
   let tr = document.createElement("tr");
   for (let i = 0; i < 20; i++) {
-    let td = document.createElement("td");
-    tr.appendChild(td);
+    let box = document.createElement("td");
+    tr.appendChild(box);
   }
   table.appendChild(tr);
 }
 let addRow = document.getElementById("add-row");
 addRow.addEventListener("click", makeRow, false);
+
+let td = document.querySelector("td")
+function changeColor(click) {
+  console.log("clicked");
+  let target = click.target;
+  target.classList.toggle("red")
+}
+table.addEventListener("click", changeColor, false);
+
+let selector = document.querySelector("select");
+
+function colorizer(change) {
+  console.log(change.target.value)
+}
+selector.addEventListener("change", colorizer, false);
